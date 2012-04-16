e775f27c5d36911d26226c290046f0b9934c2ffb
Izaak Meckler <izaakmeckler@me.com>
2012-04-15 19:56:51 -0500

    added functions for picking out popular posts

--- hackernews.py
+++ hackernews.py
@@ -4,6 +4,12 @@
 from urllib import *
 from BeautifulSoup import BeautifulSoup
 from datetime import datetime, timedelta
+from collections import defaultdict
+import string
+
+def strip_punctuation(s):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in s if ch not in exclude)
 
 def str_to_num(s):
     return int(filter(lambda c: c.isdigit(), s))
@@ -64,7 +70,49 @@ def parse_gen(n):
         yield posts
         curr_url = nextpage
 
-# example usage follows
-print "Please enter the number of pages you would like parsed."
-n = int(raw_input())
-print list(parse_gen(n))
+
+# from a list of words, generate all possible runs of length n 
+def tokenize_words(words, n):
+    tokens = []
+    for i in xrange(0, len(words) - n + 1):
+        tokens.append(words[i:i + n])
+    return tokens
+
+
+# count the frequency of each score for each token
+def token_popularities(posts, token_size):
+    token_popularity = defaultdict(lambda: defaultdict(int))
+    for post in posts:
+        title = post['title']
+        score = post['score']
+
+        if token_size == 1:
+            tokens = [w for ws in tokenize_words(strip_punctuation(title).split(), 1) for w in ws]
+        else:
+            tokens = map(tuple, tokenize_words(strip_punctuation(title).split(), 1))
+
+        for token in tokens:
+            token_popularity[token][score] += 1
+
+    return token_popularity
+
+    """
+    result = {}
+    for token, score_freq in token_pops.iteritems():
+        if not (token in bad_words or token in [w.upper() for w in bad_words]):"""
+def popular_tokens(token_pops):
+    bad_words = ['a', 'the', 'than', 'and', 'or', 'will', 'what', 'how', 'its']
+    return {token: scores for token, scores in token_pops.iteritems()
+                          if token not in bad_words and
+                             token not in [w.upper() for w in bad_words] and
+                             (max(scores) > 100 or len(scores) > 4)}
+
+def sorted_by_max_score(token_pops):
+    token_pops = {token: max(scores) for (token, scores) in token_pops.iteritems()} 
+    return sorted(token_pops.iteritems(), key=lambda (t, s): s)
+
+
+if __name__ == "__main__":
+    print "Please enter the number of pages you would like parsed."
+    n = int(raw_input())
+    print list(parse_gen(n))
